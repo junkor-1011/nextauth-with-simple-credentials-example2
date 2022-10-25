a577513d6c9b8c457a87c8f99881909d29c42890
junkor-1011 <tomoyukizxy@gmail.com>
2022-10-26 00:34:15 +0900

    token customize test

--- src/lib/auth/config.ts
+++ src/lib/auth/config.ts
@@ -10,14 +10,18 @@ const authOptions: NextAuthOptions = {
       authorize: async (credentials, _req) => {
         // DANGER: 現状のauthorizeの中身は全面的に改める必要あり
         // →ここでは概要・実現性の確認さえ出来れば良いため、実用上必要な考慮や処理を省略したり、敢えてアンチパターンな処理を入れている
-        const userExpected = "user";
-        const passwordExpected = "p@ssword";
+        const allowdCredentials = { // UNSECURE CODE: JUST FOR TEST ONLY
+          'user': 'p@ssword',
+          'dummy': ')-v-(',
+          'pochi': 'Vow!',
+          'hachi-kou': 'inu_'
+        }
 
         const username = credentials?.username;
         const password = credentials?.password;
 
         // 認証に失敗した場合はnullを返す
-        if (username !== userExpected || password !== passwordExpected)
+        if (!Object.keys(allowdCredentials).includes(username) || allowdCredentials[username] !== password)
           return null;
 
         const { createHmac } = await import('node:crypto') // TMP
@@ -50,6 +54,8 @@ const authOptions: NextAuthOptions = {
         ...session,
         customKey1: token?.customKey1,
         customKey2: token?.customKey2,
+        customKey3: token?.customKey3,
+        customKey4: token?.customKey4,
       };
 
       return sessionNew;
@@ -63,10 +69,24 @@ const authOptions: NextAuthOptions = {
       // console.log('account', account)
       // console.log('profile', profile)
 
+      // TMP
+      console.log(token)
+      const name = token?.name;
+      const dummyProp = (() =>{
+        if (name === 'user') return 'nemimini mimizu'
+        if (name === 'dummy') return 'foo bar baz'
+        if (name === 'pochi') return 'Meow!!!'
+        if (name === 'hachi-kou') return 'moai'
+
+        return 'unexpected error occurred.'
+      })();
+
       const tokenNew = {
         ...token,
         customKey1: "test string",
         customKey2: "dummy value",
+        customKey3: (new Date()).toISOString(),
+        customKey4: dummyProp,
       };
 
       return tokenNew;
